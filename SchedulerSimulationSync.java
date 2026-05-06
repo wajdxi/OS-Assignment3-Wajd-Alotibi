8b19b4be833bab3527cab4e8f739b378d60e0d31
wajdxi <445052101@std.psau.edu.sa>
2026-05-06 07:17:07 +0300

    contextSwitchCount lock

--- SchedulerSimulationSync.java
+++ SchedulerSimulationSync.java
@@ -49,11 +49,14 @@ class SharedResources {
     
     // Method to increment context switch counter
    public static void incrementContextSwitch() {
-        // TODO: Protect this critical section with a lock
-        // RACE CONDITION: Multiple threads might read and write simultaneously!
-        contextSwitchCount++;
+               contextSwitchLock.lock();
+        try {
+            contextSwitchCount++;
+        } finally {
+            contextSwitchLock.unlock();
+        }
     }
-    
+
     // Method to increment completed process counter
     public static void incrementCompletedProcess() {
         // TODO: Protect this critical section with a lock
